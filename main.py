794e32932bd9b93ee6067a3566a1a91e5b4ee7fc
developmentshoesspot-arch <developmentshoesspot@gmail.com>
2026-03-19 11:26:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import StreamingResponse
-from rembg import remove
+from rembg import remove, new_session
 import io
 
 app = FastAPI()
 
+session = new_session()
+
 @app.post("/remove-bg/")
 async def remove_bg(file: UploadFile = File(...)):
     input_image = await file.read()
-    output = remove(input_image)
+    output = remove(input_image, session=session)
     return StreamingResponse(io.BytesIO(output), media_type="image/png")
